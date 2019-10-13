283a7aee74b05fbe698ce153938e71ebc8edbc85
pilabrem <pilabrem@gmail.com>
2019-10-13 11:52:31 +0000

    Super admin, roles and permissions seeding

---
+++ database/seeds/RoleSeed.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Super Admin
+        $role = Role::create(['name' => 'super-admin']);
+        $role->givePermissionTo(Permission::all());
+
+        // Admin Simple
+        $role = Role::create(['name' => 'admin']);
+        $role->givePermissionTo(Permission::all());
+        $role->revokePermissionTo('manage administrators');
+    }
+}
