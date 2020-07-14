8f7b72211f9ee1f472a8d261644a6f0f6076df02
pilabrem <pilabrem@gmail.com>
2020-07-14 21:13:25 +0000

    Correct bug : Maximum string length error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Setting;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Builder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,13 +27,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Builder::defaultStringLength(191);
+
         // Params
         if (Schema::hasTable('settings')) {
             foreach (Setting::all() as $setting) {
-                Config::set('custom.'.$setting->key, $setting->value);
+                Config::set('custom.' . $setting->key, $setting->value);
             }
         }
-
-
     }
 }
