530a46e8167791e08734bf3dd8610ae214fbc8dc
pilabrem <pilabrem@gmail.com>
2020-07-14 20:13:52 +0000

    User management from package to App

--- routes/web.php
+++ routes/web.php
@@ -55,8 +55,6 @@ Route::group([
         ->name('settings.setting.destroy')->where('id', '[0-9]+');
 });
 
-
-
 Route::group([
     'prefix' => 'import_shema_from_xmls',
 ], function () {
@@ -66,4 +64,22 @@ Route::group([
         ->name('import_shema_from_xmls.import_shema_from_xml.create');
     Route::post('/', 'ImportShemaFromXMLsController@store')
         ->name('import_shema_from_xmls.import_shema_from_xml.store');
+});
+
+/**
+ *      Gestion des permissions et des roles
+ */
+Route::group(['middleware' => ['web', 'auth'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::resource('permissions', 'Admin\PermissionsController');
+    Route::post('permissions_mass_destroy', ['uses' => 'Admin\PermissionsController@massDestroy', 'as' => 'permissions.mass_destroy']);
+    Route::resource('roles', 'Admin\RolesController');
+    Route::post('roles_mass_destroy', ['uses' => 'Admin\RolesController@massDestroy', 'as' => 'roles.mass_destroy']);
+    Route::resource('users', 'Admin\UsersController');
+    Route::post('users_mass_destroy', ['uses' => 'Admin\UsersController@massDestroy', 'as' => 'users.mass_destroy']);
+});
+
+// Change Password Routes...
+Route::group(['middleware' => ['web']], function () {
+    Route::get('change_password', 'Auth\ChangePasswordController@showChangePasswordForm')->name('auth.change_password');
+    Route::patch('change_password', 'Auth\ChangePasswordController@changePassword')->name('auth.change_password');
 });
